965f0b79bc3bfe22efca345005a2a47a74ca6f97
xthunder94 <x.l.k@live.com>
2016-02-22 17:23:30 -0500

    Added more code rates

--- LDPC_TB.m
+++ LDPC_TB.m
@@ -1,7 +1,24 @@
 clear all; close all; clc;
 format compact;
-input = logical(randi([0 1], 324, 1));
-output = LDPC(input);
-H = dvbs2ldpc(1/2);
-K = H(1:200,1:200);
-biterr(input, output)
+LDPC(0, false, true, 1/2);
+for qam = [4 16 32 64 128 256]
+    hMod = comm.RectangularQAMModulator('ModulationOrder', qam, 'BitInput', true);
+    for snr = 0:16
+        hChan = comm.AWGNChannel('NoiseMethod', 'Signal to noise ratio (SNR)', 'SNR', snr);
+        hDemod = comm.RectangularQAMDemodulator('ModulationOrder', qam, 'BitOutput', true, ...
+                'DecisionMethod', 'Approximate log-likelihood ratio', ...
+                'Variance', 1/10^(hChan.SNR/10));
+        hError = comm.ErrorRate;
+        for counter = 1:10000
+          data           = logical(randi([0 1], 324, 1));
+          encodedData    = LDPC(data, true, false, 0);
+          modSignal      = step(hMod, encodedData);
+          receivedSignal = step(hChan, modSignal);
+          demodSignal    = step(hDemod, receivedSignal);
+          receivedBits   = LDPC(demodSignal, false, false, 0);
+          errorStats     = step(hError, data, receivedBits);
+        end
+        fprintf('Error rate       = %1.2f\nNumber of errors = %d\n', ...
+          errorStats(1), errorStats(2))
+    end
+end
