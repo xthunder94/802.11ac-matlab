000387c3cb411b251b77161157419ec1843f0813
Spellchaser <Spellchaser@users.noreply.github.com>
2016-02-22 01:57:54 -0500

    Update WiLink.m
    Added notes and possibly working file write

--- WiLink.m
+++ WiLink.m
@@ -1,19 +1,32 @@
 % BER simulation of VHT SU 802.11 ac 
 % Derived from a skeleton BER script for a wireless link simulation encased in human
 %function [ber] = WirelessLinkSimulation(MCS)
-clear all;close all;clc;format compact; MCS = 9;
+clear all;close all;clc;format compact; 
 % warnings that occur when running remotely
 warning('off','MATLAB:xlswrite:AddSheet');
 warning('off','MATLAB:xlswrite:NoCOMServer');
 
-SNR_Vec = -90:10:20; % in dB
+% Inputs
+MCS = 9;
+type = 'BCC';
+numIter = 1e2 %1e6;
+SNR_Vec = -20:5:20; % in dB
+
+% Reference Materials
 % The M-ary number, 2 corresponds to binary modulation
 msgM = [2 4 16 64 256]; 
 k = log2(msgM);   % # of information bits per symbol
 puncpat = [1; 1; 1; 0;]; % Rate 2/3 Figure 18-9
 puncpat = [1; 1; 1; 0; 0; 1;]; % Rate 3/4  Figure 18-9
 puncpat = [1; 1; 1; 0; 0; 1; 1; 0; 0; 1;]; % Rate 5/6  Figure 20-11
-puncpat = -1; 
+puncpat = -1; % Rate 1/2 Default Rate; No puncture 
+
+% Choosing which Modulation and Coding Scheme 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TODO: Functionalize this shit. Add LDPC switch cases
+% if (strcomp(type,'LDPC'))
+% else if (strcomp(type'BCC')) 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 switch MCS
     case 0 
         disp('BPSK Rate 1/2')
@@ -106,6 +119,7 @@ switch MCS
         warning('Unexpected MCS.')
 end
 
+% Math Setup for # of bits
 length_param = 4095;  % 4095 is the max LENGTH parameter. See 18.2.2.2
 N_DBPS = k;
 N_Scrambler_Init_Bits = 7;
@@ -118,12 +132,15 @@ N_PAD = N_DATA - (N_Scrambler_Init_Bits+N_Reserved_Service_Bits+...
     8 * length_param + N_Tail_bits);
 N_Punc_Pad = length(puncpat) - N_PAD - mod(N_DATA,length(puncpat)); % Padding for computation
 
+% Convolutional Encoding Setup
 constlen=7;
 codegen = [171 133]; 
 trellis = poly2trellis(constlen, codegen); % Industry standard 18.3.5.6
 htConvEnc = comm.ConvolutionalEncoder(trellis); 
 htVitDec = comm.ViterbiDecoder(trellis, 'InputFormat', 'Hard'); 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 htVitDec.TracebackDepth = 96; % TODO: find reciever TBD
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 htErrorCalc = comm.ErrorRate('ReceiveDelay', htVitDec.TracebackDepth);
 
 if (puncpat ~= -1) 
@@ -133,28 +150,10 @@ if (puncpat ~= -1)
     htVitDec.PuncturePattern = puncpat;
 end
 
-%{
-hConvEnc = comm.ConvolutionalEncoder(trellis); 
-hVitDec = comm.ViterbiDecoder(trellis, 'InputFormat', 'Unquantized');
-hVitDec.TracebackDepth = 96; % TODO: find reciever TBD
-hErrorCalc = comm.ErrorRate('ReceiveDelay', hVitDec.TracebackDepth);
-
-if (puncpat ~= -1) 
-    hConvEnc.PuncturePatternSource = 'Property';
-    hVitDec.PuncturePatternSource = 'Property';
-    hConvEnc.PuncturePattern = puncpat;
-    hVitDec.PuncturePattern = puncpat;
-end
-%}
-
 % Create a vector to store the BER computed during each iteration
 BERVec = zeros(3,length(EbNoEncoderOutput)); % Allocate memory to store results
 env_c = length(EbNoEncoderOutput);
 frameLength = N_DATA;
-numIter = 10 %1e6;
-
-% ScatterPlot! 
-%sPlotFig = scatterplot(qammod(0:msgM-1,msgM,0,'gray'),1,0,'k*');
 
 tic;
 %waitbars are invalid in parallel pools.
@@ -163,8 +162,7 @@ tic;
 % Run the simulation numIter amount of times
 % Note that using a parallel pool will not output graphs. 
 % Graphs will be generated and can be saved using print
-%parfor 
-for n=1:env_c
+parfor n=1:env_c
   %reset(hErrorCalc)
   %reset(hConvEnc)
   %reset(hVitDec)
@@ -178,8 +176,8 @@ for n=1:env_c
     % Generate binary frames of size specified by the frameLength variable
     bits = [zeros(N_Scrambler_Init_Bits+N_Reserved_Service_Bits,1); ...
         randi([0 1], frameLength-N_Scrambler_Init_Bits-N_Reserved_Service_Bits, 1); zeros(N_PAD+N_Punc_Pad,1)];
-    % Interleave the bits 
-    txdata = bits; %randintrlv(bits,sum(double('Keenesus')));
+    % Interleave the bits   % Not interleaving because parity bit math mess
+    txdata = bits; %randintrlv(bits,sum(double('Keenesus'))); 
     % Convolutionally encode the data
     encData = step(hConvEnc, txdata);
     % Modulate the encoded data
@@ -188,10 +186,9 @@ for n=1:env_c
     channelOutput = step(hChan, modData);
     % Demodulate the signal 
     demodData = step(hDeMod, channelOutput);
-    % Pass the real part of the channel complex outputs as the unquantized
-    % input to the Viterbi decoder.
+    % Pass the demodulated channel outputs as input to the Viterbi decoder
     decData = step(hVitDec, demodData);
-    % Deinterleave the bits
+    % Deinterleave the bits % Not interleaving because parity bit math mess
     data = decData; %randdeintrlv(decData,sum(double('Keenesus')));
     % Compute and accumulate errors
     BERVec(:,n) = step(hErrorCalc, bits, data);
@@ -229,4 +226,6 @@ xlswrite(filename,eqchan,'eqchan');
 filename = sprintf('snr%d_eqweights%dQAM%dfw%dbk%dk%.3fff%.2fis.csv',snr,QAM,nfwdweights,nfbkweights,Kappa,ff,is);
 xlswrite(filename,eqweights,'eqweights');
 %}
+filename = sprintf('snr%d:%d_MCS%d_Type%s.csv',snr(1),snr(end),MCS,type);
+xlswrite(filename,ber,'berVec');
 %endtime = datetime('now');
