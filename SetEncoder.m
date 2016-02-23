78765b999ae5705939280f91f159cb69eeaee4ae
T2xUS <jshih1295@gmail.com>
2016-02-23 16:58:13 -0500

    BCC Working

--- SetEncoder.m
+++ SetEncoder.m
@@ -1,11 +1,10 @@
 function [htConvEnc, htVitDec, htErrorCalc, ...
-    N_Punc_Pad, N_Pre_Pad, N_Data_Bits, N_Post_Pad] = SetEncoder(encType, debug, k, puncpat)
+    N_Pre_Pad, N_Data_Bits, N_Post_Pad] = SetEncoder(encType, debug, k, R, puncpat)
 
     if (debug == 0)
         htConvEnc = 0; % default
         htVitDec = 0; % default
         htErrorCalc = comm.ErrorRate; % for debug no encoding
-        N_Punc_Pad = 0; % default
         N_Pre_Pad = 0;
         N_Data_Bits = 1e4;
         N_Post_Pad = 0;
@@ -13,7 +12,7 @@ function [htConvEnc, htVitDec, htErrorCalc, ...
 
     elseif (strcmp(encType, 'BCC'))
         [htConvEnc, htVitDec, htErrorCalc, ...
-            N_Punc_Pad, N_Pre_Pad, N_Data_Bits, N_Post_Pad] = BCC(k, puncpat);
+             N_Pre_Pad, N_Data_Bits, N_Post_Pad] = BCC(k, R, puncpat);
 
     elseif (strcmp(encType,'LDPC'))
         LDPC(0, false, true, 1/2); % LDPC matrix initialization
@@ -21,7 +20,6 @@ function [htConvEnc, htVitDec, htErrorCalc, ...
         htVitDec = 0; % default
         htErrorCalc = comm.ErrorRate;
         code_block = 324; % Our LDPC matrices are defined for 324 fixed input
-        N_Punc_Pad = 0; % default
         N_Pre_Pad = 0;
         N_Data_Bits = code_block;
         N_Post_Pad = 0;
