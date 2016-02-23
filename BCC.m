87bd8af39be429b8f73322cdaf24e672f50abd99
T2xUS <jshih1295@gmail.com>
2016-02-23 16:57:49 -0500

    BCC Working

--- BCC.m
+++ BCC.m
@@ -1,5 +1,5 @@
 function [htConvEnc, htVitDec, htErrorCalc, ...
-    N_Punc_Pad, N_Pre_Pad, N_Data_Bits, N_Post_Pad] = BCC(k, puncpat)
+    N_Pre_Pad, N_Data_Bits, N_Post_Pad] = BCC(k, R, puncpat)
 
     % Convolutional Encoding Setup
     constlen = 7;
@@ -19,22 +19,18 @@ function [htConvEnc, htVitDec, htErrorCalc, ...
 
     % Math Setup for # of bits
     length_param = 4095;  % 4095 is the max LENGTH parameter. See 18.2.2.2
+    [numerator,denominator] = rat(R);
     N_DBPS = k;
     N_Scrambler_Init_Bits = 7;
     N_Reserved_Service_Bits = 9;
     N_Tail_bits = 6;
-    N_SYM = ceil((N_Scrambler_Init_Bits + N_Reserved_Service_Bits + ...
-        8*length_param+N_Tail_bits)/N_DBPS); % 18.3.5.4
-    N_DATA = N_SYM * N_DBPS;
+    N_SYM = ceil((N_Scrambler_Init_Bits + N_Reserved_Service_Bits+...
+        8*length_param + N_Tail_bits)/lcm(N_DBPS, length(puncpat)))*numerator; % 18.3.5.4
+    N_DATA = N_SYM * lcm(N_DBPS, length(puncpat));
     N_PAD = N_DATA - (N_Scrambler_Init_Bits + N_Reserved_Service_Bits + ...
         8 * length_param + N_Tail_bits);
-    
-    N_Punc_Pad = mod(length(puncpat) - N_PAD - mod(N_DATA,length(puncpat)), ...
-        length(puncpat)); % Padding for computation
-    N_Punc_Pad = N_Punc_Pad + mod(N_DATA + N_Punc_Pad + N_PAD + N_Tail_bits, ...
-        lcm(k,length(puncpat)));
     N_Pre_Pad = N_Scrambler_Init_Bits + N_Reserved_Service_Bits;
-    N_Data_Bits = N_DATA - N_Scrambler_Init_Bits - N_Reserved_Service_Bits;
-    N_Post_Pad = N_Tail_bits + N_PAD + N_Punc_Pad;
+    N_Post_Pad = N_Tail_bits + N_PAD;
+    N_Data_Bits = N_DATA - N_Pre_Pad - N_Post_Pad;
     
 end
