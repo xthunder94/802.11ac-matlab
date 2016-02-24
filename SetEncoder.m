380faab983462bc0a82c4c7ed1918db60f485efd
T2xUS <jshih1295@gmail.com>
2016-02-24 17:29:32 -0500

    accommodates working LDPC

--- SetEncoder.m
+++ SetEncoder.m
@@ -1,25 +1,30 @@
-function [htConvEnc, htVitDec, htErrorCalc, ...
-    N_Pre_Pad, N_Data_Bits, N_Post_Pad] = SetEncoder(encType, debug, k, R, puncpat)
+function [htEnc, htDec, htErrorCalc, ...
+    N_Pre_Pad, N_Data_Bits, N_Post_Pad] = SetEncoder(encType, debug, k, R, k_TCB, puncpat)
 
     if (debug == 0)
-        htConvEnc = 0; % default
-        htVitDec = 0; % default
+        
+        htEnc = 0; % default
+        htDec = 0; % default
         htErrorCalc = comm.ErrorRate; % for debug no encoding
+        
         N_Pre_Pad = 0;
         N_Data_Bits = 1e4;
         N_Post_Pad = 0;
         N_Data_Bits = N_Data_Bits + k - mod(N_Data_Bits,k);
 
     elseif (strcmp(encType, 'BCC'))
-        [htConvEnc, htVitDec, htErrorCalc, ...
-             N_Pre_Pad, N_Data_Bits, N_Post_Pad] = BCC(k, R, puncpat);
+        [htEnc, htDec, htErrorCalc, ...
+             N_Pre_Pad, N_Data_Bits, N_Post_Pad] = BCC(k, R, k_TCB, puncpat);
 
     elseif (strcmp(encType,'LDPC'))
-        LDPC(0, false, true, 1/2); % LDPC matrix initialization
-        htConvEnc = 0; % default
-        htVitDec = 0; % default
+        
+        % LDPC matrix initialization
+        H = LDPC(R);
+        htEnc = comm.LDPCEncoder(H);
+        htDec = comm.LDPCDecoder(H);
         htErrorCalc = comm.ErrorRate;
-        code_block = 324; % Our LDPC matrices are defined for 324 fixed input
+        
+        code_block = 648 * R; % Our LDPC matricies are defined for 648 * R fixed input
         N_Pre_Pad = 0;
         N_Data_Bits = code_block;
         N_Post_Pad = 0;
