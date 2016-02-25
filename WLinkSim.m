901d68fbba48c9bf15fabcc1ffcb23f06c045ce6
T2xUS <jshih1295@gmail.com>
2016-02-25 05:00:49 -0500

    Update WLinkSim.m

--- WLinkSim.m
+++ WLinkSim.m
@@ -1,82 +1,79 @@
-% BER simulation of VHT SU 802.11 ac 
-% Derived from a skeleton BER script for a wireless link simulation encased in human
-% function [ber] = WirelessLinkSimulation(MCS)
+% Functions that runs simulations for a single modulation scheme
 
-clear all; close all; clc; format compact;
-
-% warnings that occur when running remotely
-warning('off','MATLAB:xlswrite:AddSheet');
-warning('off','MATLAB:xlswrite:NoCOMServer');
-warning('off','MATLAB:mir_warning_maybe_uninitialized_temporary');
+function WLinkSim(MCS_in, SNR_Vec_in, encType_in, debug_in, numIter_in, h_in)
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%
-%        INPUTS         %
-%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    %        INPUTS         %
+    %%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Specify SNR range in dB
-SNR_Vec = 0:16; %0:16
+    % Specify modulation and coding scheme (ranges from 0-9)
+    MCS = MCS_in;
+    %{
+    0: BPSK Rate 1/2
+    1: QPSK Rate 1/2
+    2: QPSK Rate 3/4
+    3: 16-QAM Rate 1/2
+    4: 16-QAM Rate 3/4
+    5: 64-QAM Rate 2/3
+    6: 64-QAM Rate 3/4
+    7: 64-QAM Rate 5/6
+    8: 256-QAM Rate 3/4
+    9: 256-QAM Rate 5/6
+    %}
+    
+    SNR_Vec = SNR_Vec_in;
+    encType = encType_in;
+    debug = debug_in;
+    numIter = numIter_in;
+    h = h_in;
 
-% Specify modulation and coding scheme (ranges from 0-9)
-MCS = 9;
-%{
-0: BPSK Rate 1/2
-1: QPSK Rate 1/2
-2: QPSK Rate 3/4
-3: 16-QAM Rate 1/2
-4: 16-QAM Rate 3/4
-5: 64-QAM Rate 2/3
-6: 64-QAM Rate 3/4
-7: 64-QAM Rate 5/6
-8: 256-QAM Rate 3/4
-9: 256-QAM Rate 5/4
-%}
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    %       ENCODING        %
+    %%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Specify encoding method (BCC or LDPC)
-encType = 'BCC';
+    % Set modulation and coding scheme
+    [display, modType, lSpec, M, k, R, k_TCB, puncpat, ...
+        hMod, htDemod] = SetMCS(MCS, encType, debug);
 
-% Specify debug mode (0 if running without encoding)
-debug = 0;
+    % Set encoding method (BCC or LDPC, debug or no debug)
+    [htEnc, htDec, htErrorCalc, ...
+        N_Pre_Pad, N_Data_Bits, N_Post_Pad] = ...
+        SetEncoder(encType, debug, k, R, k_TCB, puncpat);
 
-% Specify number of iterations for simulation
-numIter = 1; %1e6
-
-%fprintf('MCS:\tType:\tIter:\t\n%d\t%s\t%d\n', MCS, encType, numIter);
-%%%%%%%%%%%%%%%%%%%%%%%%%
-%       ENCODING        %
-%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Set modulation and coding scheme
-[modType, M, k, R, k_TCB, puncpat, hMod, htDemod] = SetMCS(MCS);
-
-% Set encoding method (BCC or LDPC, debug or no debug)
-[htEnc, htDec, htErrorCalc, ...
-    N_Pre_Pad, N_Data_Bits, N_Post_Pad] = ...
-    SetEncoder(encType, debug, k, R, k_TCB, puncpat);
-
-% Run simulation and retrieve BERs
-[ber, berHypo] = Simulation(numIter, SNR_Vec, encType, debug, ...
-    modType, k, R, M, hMod, htDemod, ...
-    htEnc, htDec, htErrorCalc, ...
-    N_Pre_Pad, N_Data_Bits, N_Post_Pad)
-
-check = ((berHypo - ber(1,:)) > 0);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%
-%        GRAPHS         %
-%%%%%%%%%%%%%%%%%%%%%%%%%
-fprintf('%d\t%s\t%d\t%d\t%d\n', MCS, encType, numIter, ber(3,1),  toc);
-% Graph theoretical and actual BERs
-figure
-    if (debug ~= 0) % for encoding
-        berHypo = berawgn(SNR_Vec - 10*log10(k*R), modType, M, 'nondiff');
-    else 
-        berHypo = berawgn(SNR_Vec - 10*log10(k), modType, M, 'nondiff');
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    %  SIMULATION & GRAPHS  %
+    %%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    % Run simulation and retrieve BERs
+    [ber, berHypo] = Simulation(numIter, SNR_Vec, encType, debug, ...
+        modType, k, R, M, hMod, htDemod, ...
+        htEnc, htDec, htErrorCalc, ...
+        N_Pre_Pad, N_Data_Bits, N_Post_Pad)
+    
+    
+    % Graph theoretical or actual BERs
+    
+    name = strcat(display,{' (MCS '}, num2str(MCS), ')');
+    nameTh = strcat({'Theoretical (MCS '}, num2str(MCS), ')');
+    
+    if(debug == 0)
+        semilogy(SNR_Vec, berHypo, 'k', 'DisplayName', nameTh{1});
+        hold on
+        semilogy(SNR_Vec, ber, lSpec, 'DisplayName', name{1}); % for debug
+        hold on
+    else
+        semilogy(SNR_Vec, ber, lSpec, 'DisplayName', name{1});
+        hold on
     end
-semilogy(SNR_Vec,berHypo,'r')
-hold on
-semilogy(SNR_Vec,ber(1,:));
-hold off
-xlabel('SNR (dB)')
-legend('Theoretical BER', 'BER');
-title(strcat(num2str(M), ' ', modType));
+    
+    legend('off')
+    legend('show')
+    
+    
+    % Update waitbar
+    waitbar(MCS/9,h,sprintf('Evaluating MCS %d',MCS));
+
+end
